025402e128ac45d648937286e9018a3008a17c9d
roivasxxx <tiragoshi@gmail.com>
2024-02-04 15:25:32 +0100

    removing withAuth wrapped middleware

--- genshin-helper-app/src/middleware.ts
+++ genshin-helper-app/src/middleware.ts
@@ -1,56 +1,32 @@
-import { encode } from "next-auth/jwt";
-import { NextRequestWithAuth, withAuth } from "next-auth/middleware";
-import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
 
-const sessionCookie = process.env.NEXTAUTH_URL?.startsWith("https://")
-    ? "__Secure-next-auth.session-token"
-    : "next-auth.session-token";
-
-export default withAuth(
-    async (req: NextRequestWithAuth) => {
-        if (req.nextauth.token && req.nextauth.token.error) {
-            const isProd = process.env.NODE_ENV === "production";
-            const response = NextResponse.redirect(
-                new URL(
-                    req.url.includes(process.env.NEXTAUTH_URL + "/me")
-                        ? "/login"
-                        : "",
-                    req.url
-                )
-            );
-            response.cookies.delete("payload-token");
-            response.cookies.delete(
-                `${isProd ? "__Secure-" : ""}next-auth.session-token`
-            );
-            response.cookies.delete(
-                `${isProd ? "__Host-" : ""}next-auth.csrf-token`
-            );
-            response.cookies.delete(
-                `${isProd ? "__Secure-" : ""}next-auth.callback-url`
-            );
-            response.cookies.delete("payload-token");
-            return response;
-        }
-    },
-    {
-        callbacks: {
-            authorized: async (req) => {
-                // checks if user has token
-                return !!req.token;
-            },
-        },
-        // redirect list
-        pages: {
-            signIn: "/login",
-            error: "/login",
-            signOut: "/",
-            newUser: "/register",
-        },
+export default async function middleware(req: NextRequest) {
+    const token = await getToken({ req });
+    if (!token && req.url.includes(process.env.NEXTAUTH_URL + "/me")) {
+        return NextResponse.redirect(new URL("/login", req.url));
     }
-);
-
-//checks /me path
-export const config = {
-    matcher: ["/me/:path*"],
-};
+    if (token && token.error) {
+        const isProd = process.env.NODE_ENV === "production";
+        const response = NextResponse.redirect(
+            new URL(
+                req.url.includes(process.env.NEXTAUTH_URL + "/me")
+                    ? "/login"
+                    : "",
+                req.url
+            )
+        );
+        response.cookies.delete("payload-token");
+        response.cookies.delete(
+            `${isProd ? "__Secure-" : ""}next-auth.session-token`
+        );
+        response.cookies.delete(
+            `${isProd ? "__Host-" : ""}next-auth.csrf-token`
+        );
+        response.cookies.delete(
+            `${isProd ? "__Secure-" : ""}next-auth.callback-url`
+        );
+        response.cookies.delete("payload-token");
+        return response;
+    }
+}
