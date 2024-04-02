aadc7a9e647c1273296f2c0ac713a1464690f8c9
roivasxxx <tiragoshi@gmail.com>
2024-04-02 21:22:37 +0200

    elements endpoint

--- payloadcms/src/collections/genshin/GenshinElements.ts
+++ payloadcms/src/collections/genshin/GenshinElements.ts
@@ -1,4 +1,6 @@
-import { CollectionConfig } from "payload/types";
+import { CollectionConfig, PayloadRequest } from "payload/types";
+import authMiddleware from "../../api/authMiddleware";
+import { Response } from "express";
 
 const GenshinElements: CollectionConfig = {
     slug: "genshin-elements",
@@ -16,6 +18,31 @@ const GenshinElements: CollectionConfig = {
             type: "upload",
             relationTo: "media",
             required: true,
+        },
+    ],
+    endpoints: [
+        {
+            path: "/getElements",
+            method: "get",
+            handler: [
+                authMiddleware,
+                async (req: PayloadRequest, res: Response) => {
+                    const elements = await req.payload.find({
+                        collection: "genshin-elements",
+                    });
+
+                    const elementsMapped = elements.docs.map((el) => {
+                        const element = {
+                            name: el.name,
+                        };
+                        if (typeof el.icon === "object") {
+                            element["icon"] = el.icon.cloudinary.secure_url;
+                        }
+                        return element;
+                    });
+                    res.send(elementsMapped);
+                },
+            ],
         },
     ],
 };
